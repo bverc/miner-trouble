13801dbb45470cb4f20299e88362389e803abf72
Brendan Vercoelen <bvercoelen@gmail.com>
2012-11-03 20:36:35 +1300

    Improved obstacle avoidance

--- js/game.js
+++ js/game.js
@@ -21,35 +21,49 @@ $(document).ready(function(){
   
   //user control
   $('body').keypress(function(event) {
-    var x_new = x_loc;
-    var y_new = y_loc;
     switch (event.keyCode) {
       case 37:
-        x_new = x_loc - 1;
+        move(x_loc - 1, y_loc);
         break;
       case 38:
-        y_new = y_loc - 1;
+        move(x_loc, y_loc - 1);
         break;
       case 39:
-        x_new = x_loc + 1;
+        move(x_loc + 1, y_loc);
         break;
       case 40:
-        y_new = y_loc + 1;
+        move(x_loc, y_loc + 1);
         break;
     }
-    
+  });
+  
+  function move (x_new, y_new) {
     // push
     if ($('.y' + y_new + ' .x' + x_new).hasClass('box')) {
+      if (isObstacle(2*x_new - x_loc, 2*y_new - y_loc)) {
+        return false;
+      }
       $('.y' + y_new + ' .x' + x_new).removeClass('box');
       $('.y' + (2*y_new - y_loc) + ' .x' + (2*x_new - x_loc)).addClass('box');
     }
     
-    //check boundaries
-    if (y_new >= 0 && y_new < y_max && x_new >= 0 && x_new < x_max) {
-      $('.y' + y_loc + ' .x' + x_loc).removeClass('user');
-      $('.y' + y_new + ' .x' + x_new).addClass('user');
-      y_loc = y_new;
-      x_loc = x_new;
+    //check for obstacles
+    if (isObstacle(x_new, y_new)) {
+      return false;
     }
-  });
+    $('.y' + y_loc + ' .x' + x_loc).removeClass('user');
+    $('.y' + y_new + ' .x' + x_new).addClass('user');
+    y_loc = y_new;
+    x_loc = x_new;
+    
+    return true;
+  }
+  
+  function isObstacle (x, y) {
+    if (x < 0 || x >= x_max || y < 0 || y >= y_max) {
+      return true;
+    }
+    return false;
+  }
+
 });
